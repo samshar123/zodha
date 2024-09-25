db38a9744636e5fd3a057fb1ebfdf89dc5bad24a
samshar123 <samsharsachu1@gmail.com>
2024-09-25 12:58:44 +0530

    footer link updated

--- src/app/components/Footer/page.tsx
+++ src/app/components/Footer/page.tsx
@@ -1,54 +1,52 @@
-import styles from "../Footer/footer.module.css"
+import styles from "../Footer/footer.module.css";
 import "bootstrap/dist/css/bootstrap.css";
-import Image from "next/image"
-import zdlogo from "../../../../public/img2.png"
+import Image from "next/image";
+import zdlogo from "../../../../public/img2.png";
 import { FaLinkedin } from "react-icons/fa6";
 import { FaSquareInstagram } from "react-icons/fa6";
 import { FaFacebookSquare } from "react-icons/fa";
-export default function footer(){
-    return(
-        <section id={styles.footer}>
-            <div className="container">
-                <div className="row">
-                    <div className={`col-lg-4 ${styles.footerlogo}`}>
-                        <Image className={styles.pilogo} src={zdlogo} alt="" />
-
-                    </div>
-                    <div className={`col-lg-4 ${styles.iconsfooter}`}>
-                        <h1>Social media</h1>
-                        <div className={styles.footericons} >
-                        <a href="https://www.instagram.com/zodha_research?igsh=MWM5NGRyNHE5eGpxcQ%3D%3D&utm_source=qr"><FaSquareInstagram className={styles.footericon} /></a>   <FaFacebookSquare className={styles.footericon} /> <FaLinkedin className={styles.footericon} />
-
-                        </div>
-                    
-
-                    </div>
-                    <div className={`col-lg-4 ${styles.iconsfooter}`}>
-                        <h1>Head office</h1>
-                        <div className={styles.footericons} >
-                        <h5>RISE TBI, Sullamussalam Science College, Areekode.<br /> Kerala 673639</h5>
-
-                        </div>
-                    
-
-                    </div>
-
-                    <div className={`col-lg-12 ${styles.copyright}`}>
-                        <div className={styles.piright}>
-                            <h5>© Copyright ZODHA RESEARCH SOLUTIONS. All Rights Reserved</h5>
-
-                        </div>
-                        <div className={styles.dotright}>
-                            <h5>Developed by  <span> team dotco</span></h5>
-
-                        </div>
-
-                    </div>
-
-                </div>
-
+export default function footer() {
+  return (
+    <section id={styles.footer}>
+      <div className="container">
+        <div className="row">
+          <div className={`col-lg-4 ${styles.footerlogo}`}>
+            <Image className={styles.pilogo} src={zdlogo} alt="" />
+          </div>
+          <div className={`col-lg-4 ${styles.iconsfooter}`}>
+            <h1>Social media</h1>
+            <div className={styles.footericons}>
+              <a href="https://www.instagram.com/zodha_research?igsh=MWM5NGRyNHE5eGpxcQ%3D%3D&utm_source=qr">
+                <FaSquareInstagram className={styles.footericon} />
+              </a>{" "}
+              <FaFacebookSquare className={styles.footericon} />{" "}
+              <FaLinkedin className={styles.footericon} />
             </div>
+          </div>
+          <div className={`col-lg-4 ${styles.iconsfooter}`}>
+            <h1>Head office</h1>
+            <div className={styles.footericons}>
+              <h5>
+                RISE TBI, Sullamussalam Science College, Areekode.
+                <br /> Kerala 673639
+              </h5>
+            </div>
+          </div>
 
-        </section>
-    )
+          <div className={`col-lg-12 ${styles.copyright}`}>
+            <div className={styles.piright}>
+              <h5>© Copyright ZODHA RESEARCH SOLUTIONS. All Rights Reserved</h5>
+            </div>
+            <div className={styles.dotright}>
+              <a href="https://www.teamdotco.com/">
+                <h5>
+                  Developed by <span> team dotco</span>
+                </h5>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
